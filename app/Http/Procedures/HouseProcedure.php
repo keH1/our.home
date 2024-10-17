42fa52f482c4e71df035e05c1b5535b9448d3df3
Paul <paul.raz192@gmail.com>
2024-10-17 22:11:36 +0300

    Change Pagination builder and House methods

--- app/Http/Procedures/HouseProcedure.php
+++ app/Http/Procedures/HouseProcedure.php
@@ -27,24 +27,29 @@ class HouseProcedure extends Procedure
     public function getAllStreets(Request $request, ApiResponseBuilder $responseBuilder): array
     {
         $paginationBuilder = PaginationBuilder::fromRequest($request);
-        $limit = $paginationBuilder->getLimit();
-        $offset = $paginationBuilder->getOffset();
+        $query = House::select('street')->distinct()->orderBy('street');
 
-        $paginationBuilder->setTotal(House::distinct()->count('street'));
+        if ($paginationBuilder->isPaginationEnabled()) {
+            $limit = $paginationBuilder->getLimit();
+            $offset = $paginationBuilder->getOffset() ?? 0;
 
-        $streets = House::select('street')
-                        ->distinct()
-                        ->orderBy('street')
-                        ->limit($limit)
-                        ->offset($offset)
-                        ->pluck('street')
-                        ->toArray();
+            $paginationBuilder->setTotal((clone $query)->count());
+            $query->limit($limit)->offset($offset);
+        }
+
+        $streets = $query->pluck('street')->toArray();
 
         if (empty($streets)) {
             return $responseBuilder->setData([])->setMessage('Список улиц пуст')->build();
         }
 
-        return $responseBuilder->setData($streets)->setPagination($paginationBuilder)->build();
+        if ($paginationBuilder->isPaginationEnabled()) {
+            return $responseBuilder->setData($streets)
+                                   ->setPagination($paginationBuilder)
+                                   ->build();
+        }
+
+        return $responseBuilder->setData($streets)->build();
     }
 
     /**
@@ -67,22 +72,28 @@ class HouseProcedure extends Procedure
         $street = $validator->getValue('street');
 
         $paginationBuilder = PaginationBuilder::fromRequest($request);
-        $limit = $paginationBuilder->getLimit();
-        $offset = $paginationBuilder->getOffset();
+        $query = House::where('street', 'ILIKE', '%'.$street.'%')->orderBy('number');
 
-        $paginationBuilder->setTotal(House::where('street', 'ILIKE', '%'.$street.'%')->count());
+        if ($paginationBuilder->isPaginationEnabled()) {
+            $limit = $paginationBuilder->getLimit();
+            $offset = $paginationBuilder->getOffset() ?? 0;
 
-        $houses = House::where('street', 'ILIKE', '%'.$street.'%')
-                       ->orderBy('number')
-                       ->limit($limit)
-                       ->offset($offset)
-                       ->get();
+            $paginationBuilder->setTotal((clone $query)->count());
+            $houses = $query->limit($limit)->offset($offset)->get();
 
+            if ($houses->isEmpty()) {
+                return $responseBuilder->setData([])->setMessage('Дома по указанной улице не найдены')->build();
+            }
+
+            return $responseBuilder->setData($houses)->setPagination($paginationBuilder)->build();
+        }
+
+        $houses = $query->get();
         if ($houses->isEmpty()) {
             return $responseBuilder->setData([])->setMessage('Дома по указанной улице не найдены')->build();
         }
 
-        return $responseBuilder->setData($houses)->setPagination($paginationBuilder)->build();
+        return $responseBuilder->setData($houses)->build();
     }
 
     /**
@@ -103,32 +114,48 @@ class HouseProcedure extends Procedure
         }
 
         $houseId = $validator->validated()['house_id'];
-
         $paginationBuilder = PaginationBuilder::fromRequest($request);
-        $limit = $paginationBuilder->getLimit();
-        $offset = $paginationBuilder->getOffset();
 
-        $totalApartments = Apartment::where('house_id', $houseId)->count();
-        $paginationBuilder->setTotal($totalApartments);
-
-        $apartments = Apartment::with([
+        $query = Apartment::with([
             'counterData' => function ($query) {
                 $query->with([
                     'latestConfirmedHistory', 'latestUnconfirmedHistory',
                 ]);
             }
-        ])->where('house_id', $houseId)->orderBy('number')->limit($limit)->offset($offset)->get();
+        ])->where('house_id', $houseId)->orderBy('number');
 
+        if ($paginationBuilder->isPaginationEnabled()) {
+            $limit = $paginationBuilder->getLimit();
+            $offset = $paginationBuilder->getOffset() ?? 0;
+
+            $paginationBuilder->setTotal((clone $query)->count());
+
+            $apartments = $query->limit($limit)->offset($offset)->get();
+            if ($apartments->isEmpty()) {
+                return $responseBuilder->setData([])->setMessage('Квартиры не найдены')->build();
+            }
+
+            return $responseBuilder->setData($this->buildApartments($apartments))->setPagination($paginationBuilder)->build();
+        }
+
+        $apartments = $query->get();
         if ($apartments->isEmpty()) {
             return $responseBuilder->setData([])->setMessage('Квартиры не найдены')->build();
         }
 
-        $apartmentsData = $apartments->map(function ($apartment) {
+        return $responseBuilder->setData($this->buildApartments($apartments))->build();
+    }
+
+    private function buildApartments($apartments)
+    {
+        return $apartments->map(function ($apartment) {
             return [
-                'id' => $apartment->id, 'number' => $apartment->number,
+                'id' => $apartment->id,
+                'number' => $apartment->number,
                 'counters' => $apartment->counterData->map(function ($counter) {
                     return [
-                        'counter_id' => $counter->id, 'counter_number' => $counter->number,
+                        'counter_id' => $counter->id,
+                        'counter_number' => $counter->number,
                         'counter_type' => $counter->counter_type,
                         'verification_to' => $counter->verification_to,
                         'apartment_id' => $counter->apartment_id,
@@ -138,7 +165,8 @@ class HouseProcedure extends Procedure
                             'night_consumption' => $counter->latestConfirmedHistory->night_consumption,
                             'peak_consumption' => $counter->latestConfirmedHistory->peak_consumption,
                             'last_checked_date' => $counter->latestConfirmedHistory->last_checked_date,
-                        ] : null, 'unconfirmed_history' => $counter->latestUnconfirmedHistory ? [
+                        ] : null,
+                        'unconfirmed_history' => $counter->latestUnconfirmedHistory ? [
                             'daily_consumption' => $counter->latestUnconfirmedHistory->daily_consumption,
                             'night_consumption' => $counter->latestUnconfirmedHistory->night_consumption,
                             'peak_consumption' => $counter->latestUnconfirmedHistory->peak_consumption,
@@ -148,7 +176,5 @@ class HouseProcedure extends Procedure
                 }),
             ];
         });
-
-        return $responseBuilder->setData($apartmentsData)->setPagination($paginationBuilder)->build();
     }
 }
--- app/Services/PaginationBuilder.php
+++ app/Services/PaginationBuilder.php
@@ -9,8 +9,8 @@ use Sajya\Server\Exceptions\RpcException;
 
 class PaginationBuilder
 {
-    protected int $limit;
-    protected int $offset;
+    protected ?int $limit = null;
+    protected ?int $offset = null;
     protected int $total = 0;
 
     /**
@@ -22,20 +22,19 @@ class PaginationBuilder
      */
     public static function fromRequest(Request $request): self
     {
-        $defaultLimit = config('pagination.default_limit', 15);
-        $maxLimit = config('pagination.max_limit', 100);
+        $maxLimit = config('pagination.max_limit', 15);
 
         $validator = Validator::make($request->all(), [
             'limit' => "sometimes|integer|min:1|max:$maxLimit",
-            'offset' => 'sometimes|integer|min:0',
+            'offset' => 'required_with:limit|integer|min:0',
         ]);
 
         if ($validator->fails()) {
             throw new InvalidParams('Неверные параметры пагинации');
         }
 
-        $limit = $request['limit'] ?? $defaultLimit;
-        $offset = $request['offset'] ?? 0;
+        $limit = $request->has('limit') ? (int)$request->input('limit') : null;
+        $offset = $request->has('limit') ? (int)$request->input('offset', 0) : null;
 
         return new self($limit, $offset);
     }
@@ -43,10 +42,10 @@ class PaginationBuilder
     /**
      * Конструктор PaginationBuilder.
      *
-     * @param int $limit
-     * @param int $offset
+     * @param int|null $limit
+     * @param int|null $offset
      */
-    public function __construct(int $limit, int $offset)
+    public function __construct(?int $limit, ?int $offset)
     {
         $this->limit = $limit;
         $this->offset = $offset;
@@ -67,9 +66,9 @@ class PaginationBuilder
     /**
      * Получает значение limit.
      *
-     * @return int
+     * @return int|null
      */
-    public function getLimit(): int
+    public function getLimit(): ?int
     {
         return $this->limit;
     }
@@ -77,20 +76,34 @@ class PaginationBuilder
     /**
      * Получает значение offset.
      *
-     * @return int
+     * @return int|null
      */
-    public function getOffset(): int
+    public function getOffset(): ?int
     {
         return $this->offset;
     }
 
     /**
+     * Проверяет, активна ли пагинация.
+     *
+     * @return bool
+     */
+    public function isPaginationEnabled(): bool
+    {
+        return $this->limit !== null;
+    }
+
+    /**
      * Возвращает данные пагинации для ответа.
      *
-     * @return array
+     * @return array|null
      */
-    public function toArray(): array
+    public function toArray(): ?array
     {
+        if (!$this->isPaginationEnabled()) {
+            return null;
+        }
+
         return [
             'total' => $this->total,
             'limit' => $this->limit,
