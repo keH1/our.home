282b6af5e81aeecdca46c9bbbd2f3bb22ff02e5d
Tsarev Pavel <czar_hot@mail.ru>
2024-10-20 19:30:18 +0300

    fixes for 1c customers and filter for counters

--- app/Models/CounterData.php
+++ app/Models/CounterData.php
@@ -15,7 +15,7 @@ class CounterData extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name', 'number', 'apartment_id', 'verification_to', 'counter_type', 'counter_seal', 'factory_number','personal_number'
+        'name', 'number', 'apartment_id', 'verification_to', 'counter_type', 'counter_seal', 'factory_number', 'personal_number'
     ];
 
     protected $casts = [
@@ -29,7 +29,7 @@ class CounterData extends Model
 
     public function histories(): HasMany
     {
-        return $this->hasMany(CounterHistory::class,'counter_name_id');
+        return $this->hasMany(CounterHistory::class, 'counter_name_id');
     }
 
     public function clients(): BelongsToMany
@@ -37,7 +37,7 @@ class CounterData extends Model
         return $this->belongsToMany(AccountPersonalNumber::class, 'counter_data_account_id');
     }
 
-    public function accounts():BelongsTo
+    public function accounts(): BelongsTo
     {
         return $this->belongsTo(AccountPersonalNumber::class);
     }
@@ -50,38 +50,35 @@ class CounterData extends Model
         $previousMonthEnd = now()->subMonth()->endOfMonth();
 
         return $this->hasOne(CounterHistory::class, 'counter_name_id')->where(
-                function ($query) use ($currentMonthStart, $currentMonthEnd, $previousMonthStart, $previousMonthEnd) {
-                    $query->where(function ($subQuery) use ($currentMonthStart, $currentMonthEnd) {
-                        $subQuery->where('from_1c', false)->where('approved', true)->whereBetween(
-                                'last_checked_date',
-                                [$currentMonthStart, $currentMonthEnd]
-                            );
-                    })->orWhere(function ($subQuery) use ($previousMonthStart, $previousMonthEnd) {
-                            $subQuery->where('from_1c', true)->whereBetween(
-                                    'last_checked_date',
-                                    [$previousMonthStart, $previousMonthEnd]
-                                );
-                        });
-                }
-            )->orderByRaw(
-                "
+            function ($query) use ($currentMonthStart, $currentMonthEnd, $previousMonthStart, $previousMonthEnd) {
+                $query->where(function ($subQuery) use ($currentMonthStart, $currentMonthEnd) {
+                    $subQuery->where('from_1c', false)->where('approved', true)->whereBetween(
+                        'last_checked_date',
+                        [$currentMonthStart, $currentMonthEnd]
+                    );
+                })->orWhere(function ($subQuery) {
+                    $subQuery->where('from_1c', true);
+                });
+            }
+        )->orderByRaw(
+            "
                     CASE
                         WHEN from_1c = false AND approved = true THEN 0
                         WHEN from_1c = true THEN 1
                         ELSE 2
                     END
                 "
-            )->latest('last_checked_date');
+        )->latest('last_checked_date');
     }
 
     public function latestUnconfirmedHistory()
     {
         return $this->hasOne(CounterHistory::class, 'counter_name_id')
-                    ->where(['from_1c' => false,'approved'=>false])
-                    ->whereBetween('last_checked_date', [
-                        now()->subMonth()->startOfMonth(),
-                        now()->subMonth()->endOfMonth()
-                    ])
-                    ->latest('last_checked_date');
+            ->where(['from_1c' => false, 'approved' => false])
+            ->whereBetween('last_checked_date', [
+                now()->subMonth()->startOfMonth(),
+                now()->subMonth()->endOfMonth()
+            ])
+            ->latest('last_checked_date');
     }
 }
